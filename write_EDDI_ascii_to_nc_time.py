fca98509a446a10e7f084d70892230e4ce8d8894
@kcounts <kevin.counts@noaa.gov>
2024-11-15 17:29:27 -0700

    Data conversion updates to build netcdf structure correctly. The new script write_EDDI_ascii_to_nc_time.nc does the same thing as write_EDDI_ascii_nc.py but includes a time coordinate calculated from the date and interval listed in the ascii file name.

--- write_EDDI_ascii_to_nc_time.py
+++ write_EDDI_ascii_to_nc_time.py
@@ -9,9 +9,17 @@ import pandas as pd
 def ascii_to_netcdf(ascii_file, netcdf_file):
 
     #Parse date and time-scale parameters from filename
+    TStype=ascii_file[-15:-13]
+    TSnum=ascii_file[-17:-15]
     datestr= ascii_file[-12:-4]
     date=datetime.datetime.strptime(datestr,'%Y%m%d')
-  
+    if TStype=='mn':
+        EDDIdate = date - datetime.timedelta(days=(30*int(TSnum)))
+    elif TStype=='wk':
+        EDDIdate = date - datetime.timedelta(days=(7*int(TSnum)))
+    else:
+        print('error, timescale type not recognized')   
+
     #Read in EDDI header data
     with open(ascii_file, 'r') as EDDI_f:
         EDDI_header = EDDI_f.readlines()[:6]
@@ -29,11 +37,20 @@ def ascii_to_netcdf(ascii_file, netcdf_file):
     #Create longitude and latitude arrays with appropriate values
     lon_array=np.linspace(EDDI_xll,EDDI_xll+(EDDI_cs*EDDI_cols),EDDI_cols)
     lat_array=np.linspace(EDDI_yll,EDDI_yll+(EDDI_cs*EDDI_rows),EDDI_rows)
+    
+    #Calculate datetime variables
+    date_origin = pd.to_datetime('1900-01-01 00:00:00')
+    time=[(EDDIdate - date_origin)/np.timedelta64(1,'h')]
 
     nc = netCDF4.Dataset(netcdf_file,'w',format='NETCDF4_CLASSIC')
     #Create the dimensions:
+    nc.createDimension('time',1)
     nc.createDimension('lat',EDDI_rows)
     nc.createDimension('lon',EDDI_cols)
+
+    time_init_nc = nc.createVariable('time','i4',('time',))
+    time_init_nc.long_name = 'EDDI date'
+    time_init_nc.units = "hours since 1900-01-01 00:00:00"
     
     longitude_nc = nc.createVariable('lon','f8',('lon',))
     longitude_nc.long_name = 'Longitude'
@@ -44,14 +61,15 @@ def ascii_to_netcdf(ascii_file, netcdf_file):
     latitude_nc.long_name = 'Latitude'
     latitude_nc.units = 'degrees_north'
 
-    variable_nc = nc.createVariable('EDDI','f8',('lat','lon',), zlib=True)
+    variable_nc = nc.createVariable('EDDI','f8',('time','lat','lon',), zlib=True)
     variable_nc.long_name = "Z-score, weekly EDDI"
     variable_nc.units = " "
 
     #Write data:
+    time_init_nc[:] = time
     latitude_nc[:] = lat_array
     longitude_nc[:] = lon_array
-    variable_nc[:,:] = data
+    variable_nc[:,:,:] = data
 
     ## Attributes of the NetCDF:
     nc.Name = 'EDDI for '+date.strftime('%Y/%m/%d')+'.'
@@ -66,7 +84,7 @@ if __name__ == "__main__":
     #input_directory = 'path/to/ascii/files'
     #output_directory = 'path/to/output/netcdf'
     input_directory = '/Users/kcounts/eddi/EDDI_data/'
-    output_directory = '/Users/kcounts/eddi/EDDI_data/netcdf'
+    output_directory = '/Users/kcounts/eddi/EDDI_data/netcdf/netcdf_time'
     #Read date from command line argument
     #datestr=sys.argv[1]
     datestr='20240606'
